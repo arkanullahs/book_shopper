d4cf4539d1f554659e8ce7bdaa6795fee31b6f59
“arkanullahs” <arkanullah.cse.20220104011@aust.edu>
2023-11-29 15:07:18 +0600

    Fixed all bugs,now every card shows pictures accurately. Added some more enhancements to the UI.

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,10 @@ class BookApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.brown, // Change this to your desired color
+      ),
       home: BookListPage(),
     );
   }
